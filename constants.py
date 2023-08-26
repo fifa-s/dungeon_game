2679f00581a4af2e1d9a8e61676e0bb56df89756
fifa-s <120599783+fifa-s@users.noreply.github.com>
2023-08-26 16:47:47 +0000

    -

--- constants.py
+++ constants.py
@@ -1,8 +1,10 @@
 """All major constants that affect the whole game should be defined here"""
 
 import platform
+from pygame import transform
 
 SYSTEM = platform.system()
+SCALE_FUNCTION = transform.smoothscale
 
 SCREEN_SIZE = (1920, 1080)
 DISPLAY_SIZE = (480, 270) # Not so useful, just the size the screen resizes to when you minimize IT
